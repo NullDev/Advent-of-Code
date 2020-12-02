12f2214eee436eaf89f1ae149397d388a4eb5aa3
NullDev (Chris) <shadow@nulldev.org>
2020-12-02 13:26:45 +0100

    added part_2

--- Day_1/README.md
+++ Day_1/README.md
@@ -27,6 +27,8 @@ In this list, the two entries that sum to `2020` are `1721` and `299`. Multiplyi
 
 Of course, your expense report is much larger. **Find the two entries that sum to `2020`; what do you get if you multiply them together?**
 
+---
+
 ## --- Part Two ---
 
 The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find **three** numbers in your expense report that meet the same criteria.
---
+++ Day_1/part_2.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+
+let contentArr = String(fs.readFileSync(path.join(".", "input.txt"))).split("\n").map(Number);
+
+/**
+ * Get the sum of three array elements
+ *
+ * @param {Array} array
+ * @param {Number} target
+ * @returns {Array} elements
+ */
+let threeSum = function(array, target){
+    array.sort((a,b) => a - b);
+    const triplets = [];
+
+    for (let i = 0; i < array.length - 2; i++){
+        if (array[i] != array[i-1]){
+            let left = i + 1;
+            let right = array.length - 1;
+
+            while (left < right){
+                let currentSum = array[i] + array[left] + array[right];
+                if (currentSum === target){
+                    triplets.push([array[i], array[left], array[right]]);
+                    while (array[left] == array[left + 1]) left++
+                    while (array[right] == array[right - 1]) right-- 
+                    left ++;
+                    right --;
+                } 
+                else if (currentSum < target) left++
+                else if (currentSum > target) right--
+            }
+        }
+    }
+    return triplets
+};
+
+let res = threeSum(contentArr, 2020);
+console.log(res[1].reduce((p, c) => p*c));
