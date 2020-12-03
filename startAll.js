ad9e54ca0f639829833b8b0a6d6d13f9097beebf
NullDev (Chris) <shadow@nulldev.org>
2020-12-03 16:31:27 +0100

    dont bother around with hrtime

--- startAll.js
+++ startAll.js
@@ -7,6 +7,7 @@
 let fs = require("fs");
 let path = require("path");
 let { execSync } = require("child_process");
+let { performance } = require("perf_hooks");
 
 const BC = "\x1b[42m\x1b[30m ✓ \x1b[0m\x1b[32m ";
 const MC = "\x1b[32m - \x1b[33m(took ";
@@ -15,13 +16,17 @@ const DIRECTORIES = fs.readdirSync(path.join(__dirname), { withFileTypes: true }
     .filter(dirEnt => dirEnt.isDirectory() && String(dirEnt.name).toLowerCase().includes("day_"))
     .map(dirEnt => dirEnt.name);
 
-/**
- * Calculate MS from HR-Time
- *
- * @param {[number, number]} hrtime
- * @returns {Number} ms
- */
-let hrt2ms = (hrtime) => Math.round(((hrtime[0] * 1e9) + hrtime[1]) / 1e6);
+console.log(
+`\x1b[32m █████╗  ██████╗  ██████╗
+██╔══██╗██╔═══██╗██╔════╝
+███████║██║   ██║██║     
+██╔══██║██║   ██║██║     
+██║  ██║╚██████╔╝╚██████╗
+╚═╝  ╚═╝ ╚═════╝  ╚═════╝
+\x1b[33m---= \x1b[36mAdvent of Code\x1b[33m =---
+- \x1b[0mSolutions by \x1b[32mNullDev\x1b[33m -
+------------------------\x1b[0m\n`
+);
 
 DIRECTORIES.forEach((element, index) => {
     let day = String(++index).padStart(2, "0");
@@ -31,14 +36,14 @@ DIRECTORIES.forEach((element, index) => {
 
     console.log(`\x1b[36m---====[ DAY ${day} ]====---\x1b[0m\n`);
 
-    let part1StartTime = process.hrtime();
+    let part1S = performance.now();
     let part1Out = execSync("node " + PART1);
-    let part1Time = hrt2ms(process.hrtime(part1StartTime));
+    let part1E = performance.now();
 
-    let part2StartTime = process.hrtime();
+    let part2S =  performance.now();
     let part2Out = execSync("node " + PART2);
-    let part2Time = hrt2ms(process.hrtime(part2StartTime));
+    let part2E = performance.now();
 
-    console.log(BC + "PART 1: \x1b[0m" + String(part1Out).replace(/\r?\n|\r/g, "") + MC + "\x1b[31m" + part1Time + "\x1b[33m ms)\x1b[0m");
-    console.log(BC + "PART 2: \x1b[0m" + String(part2Out).replace(/\r?\n|\r/g, "") + MC + "\x1b[31m" + part2Time + "\x1b[33m ms)\x1b[0m\n");
+    console.log(`${BC}PART 1: \x1b[0m${String(part1Out).replace(/\r?\n|\r/g, "")}${MC}\x1b[31m${Math.round(part1E - part1S)}\x1b[33m ms)\x1b[0m`);
+    console.log(`${BC}PART 2: \x1b[0m${String(part2Out).replace(/\r?\n|\r/g, "")}${MC}\x1b[31m${Math.round(part2E - part2S)}\x1b[33m ms)\x1b[0m\n`);
 });
