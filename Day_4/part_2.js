abb6f4f9994a0cbf6c0123dce82464f3fde03fe2
NullDev (Chris) <shadow@nulldev.org>
2020-12-04 13:31:12 +0100

    cleanup by removing branching

--- Day_4/part_2.js
+++ Day_4/part_2.js
@@ -10,7 +10,7 @@ let path = require("path");
 let VALID = 0;
 
 const REQUIRED_PROPERTIES = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-const EYE_COLORS = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+const VALID_EYE_COLORS = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
 
 /**
  * Check is the persons passport is valid
@@ -19,23 +19,21 @@ const EYE_COLORS = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
  * @returns {Boolean} valid/invalid
  */
 let isValid = function(element){
+    let num = Number((element.hgt).replace(/in/gi, "").replace(/cm/gi, ""));
+
     let byrValid = (Number(element.byr) >= 1920 && Number(element.byr) <= 2002);
     let iyrValid = (Number(element.iyr) >= 2010 && Number(element.iyr) <= 2020);
     let eyrValid = (Number(element.eyr) >= 2020 && Number(element.eyr) <= 2030);
     let hclValid = (/^#[0-9A-F]{6}$/gi.test(element.hcl));
-    let eclValid = (EYE_COLORS.includes(element.ecl));
+    let eclValid = (VALID_EYE_COLORS.includes(element.ecl));
     let pidValid = ((element.pid).length === 9 && (typeof Number(element.pid) === "number") && !isNaN(Number(element.pid)));
-
-    let hgtValid = false;
-    if (/^([0-9]*)(in|cm)$/gi.test(element.hgt)){
-        let num = Number((element.hgt).replace(/in/gi, "").replace(/cm/gi, ""));
-        if ((element.hgt).toLowerCase().includes("cm")){
-            if (num >= 150 && num <= 193) hgtValid = true;
-        }
-        else if ((element.hgt).toLowerCase().includes("in")){
-            if (num >= 59 && num <= 76) hgtValid = true;
-        }
-    }
+    let hgtValid = (
+        /^([0-9]*)(in|cm)$/gi.test(element.hgt) &&
+        element.hgt.toLowerCase().includes("cm") && 
+        150 <= num && 193 >= num || 
+        element.hgt.toLowerCase().includes("in") && 
+        59 <= num && 76 >= num
+    );
 
     return (byrValid && iyrValid && eyrValid && hclValid && eclValid && pidValid && hgtValid);
 };
