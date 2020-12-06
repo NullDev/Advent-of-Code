f80cb1b86cb8d57c9d0abcae719d9e3ea51f11a4
NullDev (Chris) <shadow@nulldev.org>
2020-12-06 17:57:33 +0100

    refactored part 2 (16.5ms -> 3.8ms)

--- Day_6/part_2.js
+++ Day_6/part_2.js
@@ -12,18 +12,16 @@ const CONTENT_READ = String(fs.readFileSync(path.join(__dirname, "input.txt")));
 
 const pStart = performance.now();
 
-const UNIQUE = CONTENT_READ.split(require("os").EOL)
+const UNIQUE = CONTENT_READ.replace(/\n\r/g, "\n")
+    .replace(/\r/g, "\n")
+    .split(/\n{2,}/g)
     .map(l => l.trim())
-    .reduce((temp, person) => (person.length ? temp[temp.length - 1].push(person) && temp : [...temp, []]), [[]])
-    .map(group => {
-        const answers = {};
-        Array.from(group.join("")).forEach(answer => (answers[answer] = answers[answer] ? answers[answer] + 1 : 1));
-        return Object.keys(answers).filter(a => answers[a] === group.length).length;
-    });
-
-const RES = UNIQUE.reduce((temp, group) => (temp += group), 0);
+    .reduce((count, group) => {
+		const p = group.split(/\s+/).map((x) => [...x]);
+		return count + p[0].filter((a) => p.every((as) => as.includes(a))).length;
+	}, 0);
 
 const pEnd = performance.now();
 
-console.log("TOTAL COUNT: " + RES);
+console.log("TOTAL COUNT: " + UNIQUE);
 console.log(pEnd - pStart);
