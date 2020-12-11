1314efb11f3bfca2ef9f10156f3c75414f7c986c
NullDev (Chris) <shadow@nulldev.org>
2020-12-11 20:33:52 +0100

    Added solution to day 11 part 2 (kinda)

---
+++ Day_11/part_2.js
@@ -0,0 +1,56 @@
+"use strict";
+
+// ========================= //
+// = Copyright (seat) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const CONTENT_READ = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split(require("os").EOL);
+
+const pStart = performance.now();
+
+// https://en.wikipedia.org/wiki/Adjacency_matrix
+const directionMatrix = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]];
+
+let currentState = CONTENT_READ.map((line) => line.trim().split(""));
+let layoutChanged = true;
+
+do {
+    layoutChanged = false;
+    // eslint-disable-next-line no-loop-func
+    let nextState = currentState.map((row, rowIndex) => row.map((seat, index) => {
+        let count = 0;
+
+        for (let [dx, dy] of directionMatrix){
+            let xy = [index, rowIndex];
+            do {
+                xy[0] += dx;
+                xy[1] += dy;
+                if (currentState[xy[1]] && currentState[xy[1]][xy[0]] !== "."){
+                    if (currentState[xy[1]] && currentState[xy[1]][xy[0]] === "#") count++;
+                    break;
+                }
+            }
+            while (currentState[xy[1]] && currentState[xy[1]][xy[0]]);
+        }
+
+        if (seat === "L" && count === 0 || seat === "#" && count >= 5){
+            layoutChanged = true;
+            return seat === "L" ? "#" : "L";
+        }
+
+        return seat;
+    }));
+    currentState = nextState;
+}
+while (layoutChanged);
+
+let res = currentState.reduce((count, row) => row.filter(seat => seat === "#").length + count, 0);
+
+const pEnd = performance.now();
+
+console.log("OCCUPIED SEATS: " + res);
+console.log(pEnd - pStart);
