75ad661f3ee71f1ccc8d9636c8661d9d24bcd0c9
NullDev (Chris) <shadow@nulldev.org>
2020-12-14 13:59:58 +0100

    added solution to day 14 part 1

---
+++ Day_14/part_1.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const CONTENT_READ = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split(require("os").EOL).filter(e => !!e);
+
+const pStart = performance.now();
+
+const mem = new Map();
+let mask;
+
+CONTENT_READ.forEach(line => {
+    if (line.startsWith("mask")) [, mask] = line.split(" = ");
+    else {
+        const [, address, value] = line.match(/mem\[(\d+)\] = (\d+)/);
+        mem.set(address, parseInt(Number(value)
+            .toString(2)
+            .padStart(36, "0")
+            .split("")
+            .map((bit, i) => (mask[i] === "X" ? bit : mask[i]))
+            .join(""), 2
+        ));
+    }
+});
+
+let res = Array.from(mem.values()).reduce((p, c) => p + c);
+
+const pEnd = performance.now();
+
+console.log("SUM OF MEMORY VALUES: " + res);
+console.log(pEnd - pStart);
