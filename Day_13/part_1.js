f4d1a993aa6437f6c2269491106623f543245c53
NullDev (Chris) <shadow@nulldev.org>
2020-12-14 18:24:51 +0100

    refactoring

--- Day_13/part_1.js
+++ Day_13/part_1.js
@@ -12,13 +12,11 @@ const CONTENT_READ = String(fs.readFileSync(path.join(__dirname, "input.txt"))).
 
 const pStart = performance.now();
 
-const TIMESTAMP = Number(CONTENT_READ[0]);
-
 const RES = CONTENT_READ[1]
     .split(",")
     .filter(e => e !== "x")
     .map(Number)
-    .map(id => [id, id - (TIMESTAMP % id)])
+    .map(id => [id, id - (Number(CONTENT_READ[0]) % id)])
     .sort((a, b) => a[1] - b[1])[0]
     .reduce((p, c) => p * c);
 
