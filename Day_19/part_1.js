37530e0a15e2e3cfc02ad48d0db9924f7dccebe5
NullDev (Chris) <shadow@nulldev.org>
2020-12-19 17:55:49 +0100

    added solutions to part 1 of day 19

---
+++ Day_19/part_1.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+/* eslint-disable no-nested-ternary */
+
+let fs = require("fs");
+let path = require("path");
+let EOL = require("os").EOL;
+let { performance } = require("perf_hooks");
+
+const CONTENT_READ = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split(EOL.repeat(2));
+
+const pStart = performance.now();
+
+let RES = 0;
+
+const rules = new Map();
+
+CONTENT_READ[0].split(EOL).map(e => rules.set(Number(e.split(": ")[0]), e.split(": ")[1]));
+
+const r = new RegExp("^" + (function defineRules(ruleset, rule, n = 0){
+    return n > 4 ? "" : rule.startsWith('"')
+        ? rule.slice(1, -1)
+        : "(?:" + rule.split(" | ").map(e => e.split(" ")
+            .map(f => defineRules(ruleset, ruleset.get(Number(f)), ruleset.get(Number(f)) === rule ? n + 1 : 0))
+            .join("")
+        ).join("|") + ")";
+})(rules, rules.get(0), 0) + "$");
+
+// This could literally be a two-liner by just inserting "r"
+// directly here. However due to the additional computations,
+// the total execution time increases from about ~13ms to ~365ms!
+// Thus, I left it split up...
+CONTENT_READ[1].split(EOL).forEach(e => (r.test(e) && (RES++)));
+
+const pEnd = performance.now();
+
+console.log("MATCHING MESSAGES: " + RES);
+console.log(pEnd - pStart);
