ec809d62d8a2e9c4d110b601009ad799cda69e49
NullDev (Chris) <shadow@nulldev.org>
2020-12-20 17:33:44 +0100

    refactored a bit

--- Day_20/part_1.js
+++ Day_20/part_1.js
@@ -6,22 +6,23 @@
 
 let fs = require("fs");
 let path = require("path");
+let EOL = require("os").EOL;
 let { performance } = require("perf_hooks");
 
-const CONTENT_READ = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trimEnd().split("\n\n");
+const CONTENT_READ = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trimEnd().split(`${EOL}${EOL}`);
 
 const pStart = performance.now();
 
-const RES = Object.values(CONTENT_READ.reduce((acc, tile) => {
-    let tileGrid = tile.split("\n").slice(1).map(w => w.split(""));
-    [tileGrid[0], tileGrid.map(r => r[0]), tileGrid.map(r => r[tileGrid.length - 1]), tileGrid[tileGrid.length - 1]].forEach(b => {
+const RES = Object.values(CONTENT_READ.reduce((prev, tile) => {
+    let grid = tile.split("\n").slice(1).map(w => w.split(""));
+    [grid[0], grid.map(r => r[0]), grid.map(r => r[grid.length - 1]), grid[grid.length - 1]].forEach(b => {
         let t = [b, ([...b].reverse())].sort()[0];
-        acc[t] = typeof(acc[t]) === "undefined" 
+        prev[t] = typeof(prev[t]) === "undefined" 
             ? [].concat(Number(tile.match(/^Tile (\d+):\n/)[1]))
-            : acc[t].concat(Number(tile.match(/^Tile (\d+):\n/)[1]));
+            : prev[t].concat(Number(tile.match(/^Tile (\d+):\n/)[1]));
     });
 
-    return acc;
+    return prev;
 }, {})).filter(ids => ids.length === 1) // @ts-ignore
     .flat()
     .filter((id, index, ids) => ids.indexOf(id) !== index)
