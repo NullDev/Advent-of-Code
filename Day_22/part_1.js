c17be8682887ef2a5c2dc5b90666b728959cbc03
NullDev (Chris) <shadow@nulldev.org>
2020-12-23 11:37:50 +0100

    Added solution to part 1 of day 22

---
+++ Day_22/part_1.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const CONTENT_READ = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trimEnd();
+
+const pStart = performance.now();
+
+const cardDecks = CONTENT_READ.match(/(\n\d+)+/g).map(e => e.trim().split("\n").map(n => Number(n)).reverse());
+for (let i = 1, s = []; cardDecks[0].length && cardDecks[1].length; i++){
+    s = [].concat(cardDecks[0].pop(), cardDecks[1].pop());
+    s[0] > s[1] ? cardDecks[0].unshift(...s.reverse()) : cardDecks[1].unshift(...s);
+}
+const RES = (cardDecks[0].length ? cardDecks[0] : cardDecks[1]).reduce((prev, curr, ind) => prev + curr * (ind + 1), 0);
+
+const pEnd = performance.now();
+
+console.log("WINNING SCORE: " + RES);
+console.log(pEnd - pStart);
