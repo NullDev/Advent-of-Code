42512d08ec773b2a5aa53c410387b06f3178e4f6
NullDev (Chris) <shadow@nulldev.org>
2020-12-25 17:23:40 +0100

    Added """solution""" to day 24 part 2

--- Day_24/part_1.js
+++ Day_24/part_1.js
@@ -28,7 +28,7 @@ const hMatrix = {
 const tiles = new Set();
 
 CONTENT_READ.forEach(line => {
-    let xy = [0, 0];
+    let xy = [0, 0]; // @ts-ignore
     [...line.matchAll(/e|se|sw|w|nw|ne/g)].map(e => e[0]).forEach(dir => (xy = [xy[0] + hMatrix[dir].dx, xy[1] + hMatrix[dir].dy]));
     tiles.has(xy[0] + "#" + xy[1]) ? tiles.delete(xy[0] + "#" + xy[1]) : tiles.add(xy[0] + "#" + xy[1]);
 });
