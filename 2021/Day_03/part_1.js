614686fdeec86dabd6861a3441c4ed1e2b9a17d1
NullDev (Chris) <shadow@nulldev.org>
2021-12-04 19:53:24 +0100

    Finished day 3 part 1

---
+++ 2021/Day_03/part_1.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split(require("os").EOL);
+
+const pStart = performance.now();
+
+let γ = "";
+
+for (let i = 0; i < INPUT[0].length; i++){
+    γ += (INPUT.reduce((prev, line) => prev + Number(Number(line[i]) === 1), 0) < INPUT.length / 2 ? "0" : "1");
+}
+
+const RES = parseInt(γ, 2) * parseInt(γ.split("").reduce((prev, n) => prev + (n === "1" ? "0" : "1"), ""), 2);
+
+const pEnd = performance.now();
+
+console.log("POWER CONSUMPTION: " + RES);
+console.log(pEnd - pStart);
