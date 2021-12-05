44cb8cfec467009bb1b4b7828a579446848cec8d
NullDev (Chris) <shadow@nulldev.org>
2021-12-05 19:55:39 +0100

    solution to day 5 part 1

---
+++ 2021/Day_05/part_1.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim()
+    .split(require("os").EOL)
+    .map(line => line.split(" -> ")
+        .map(coord => coord.split(",").map(Number))
+    );
+
+const pStart = performance.now();
+
+let RES = 0;
+const points = {};
+
+let check = function(point){
+    if (!points[point]) points[point] = 1;
+    else if (points[point] === 1) (RES++) && (points[point]++);
+};
+
+INPUT.forEach(([[x1, y1], [x2, y2]]) => {
+    if (x1 === x2 && y1 === y2) check(x1 + "," + y1);
+    else if (x1 === x2) for (let y = Math.min(y1, y2); y <= Math.max(y1, y2); y++) check(x1 + "," + y);
+    else if (y1 === y2) for (let x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) check(x + "," + y1);
+});
+
+const pEnd = performance.now();
+
+console.log("POINTS: " + RES);
+console.log(pEnd - pStart);
