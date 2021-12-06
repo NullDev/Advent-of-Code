7635b865925f610f744ea88bea828d0077a29740
NullDev <shadow@nulldev.org>
2021-12-06 13:55:36 +0100

    day 6 done

---
+++ 2021/Day_06/part_2.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split(",").map(Number);
+const F = new Array(9).fill(0);
+
+const pStart = performance.now();
+
+INPUT.forEach(f => (F[f]++));
+
+for (let d = 0; d < 256; d++){
+    const f = F.shift();
+    F.push(f);
+    F[6] += f;
+}
+
+const RES = F.reduce((a, b) => a + b, 0);
+
+const pEnd = performance.now();
+
+console.log("LANTERNFISH COUNT (256 DAYS): " + RES);
+console.log(pEnd - pStart);
