3e2ede046a8387937c498edecfcb03c2ce07946a
NullDev // Chris <shadow@nulldev.org>
2021-12-07 13:15:00 +0100

    Update part_1.js

--- 2021/Day_06/part_1.js
+++ 2021/Day_06/part_1.js
@@ -13,6 +13,22 @@ const F = new Array(9).fill(0);
 
 const pStart = performance.now();
 
+/**
+ * NOTE:
+ * This can also be solved by using this Matrix: 
+ * 000000101
+ * 100000000
+ * 010000000
+ * 001000000
+ * 000100000
+ * 000010000
+ * 000001000
+ * 000000100
+ * 000000010
+ * and multiplying it with itself 80 times, 
+ * then multiplying the resulting matrix by the input vector.
+ */
+
 INPUT.forEach(f => (F[f]++));
 
 for (let d = 0; d < 80; d++){
