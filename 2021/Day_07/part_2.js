9b198d29a7950faa7cd7676816c4c85a166fe9f1
NullDev (Chris) <shadow@nulldev.org>
2021-12-07 11:47:24 +0100

    added comment

--- 2021/Day_07/part_2.js
+++ 2021/Day_07/part_2.js
@@ -15,6 +15,11 @@ const LIST = [];
 
 const pStart = performance.now();
 
+// c * (c + 1) / 2
+// can be written as
+// (1 / 8) * (((2 * c) + 1) ** 2) - (1 / 8)
+// c in this example is Math.abs(INPUT[j] - i)
+
 for (let i = 0; i < Math.max(...INPUT); i++) ((LIST[i] = 0) || 1) && INPUT.forEach((_, j) => (LIST[i] += (1 / 8) * (((2 * Math.abs(INPUT[j] - i)) + 1) ** 2) - (1 / 8)));
 
 const RES = Math.min(...LIST);
