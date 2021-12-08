9c68865d3ec585c2cc96ff68ac7ebbf2cf496650
NullDev (Chris) <shadow@nulldev.org>
2021-12-08 02:38:37 +0100

    2019 Day 4

---
+++ 2019/Day_04/part_1.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("-").map(Number);
+
+const pStart = performance.now();
+
+const RES = (new Array(INPUT[1] - INPUT[0]))
+    .fill(0)
+    .map((_, i) => i + INPUT[0])
+    .map(String)
+    .filter(p => !(!p.match(/(\d)\1/g) || p.split("").sort().join("") !== p))
+    .length;
+
+const pEnd = performance.now();
+
+console.log("PASSWORD COUNT: " + RES);
+console.log(pEnd - pStart);
