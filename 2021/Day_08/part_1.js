175c6ace611995b16d3c2dcb8e1406627e45a8f8
NullDev (Chris) <shadow@nulldev.org>
2021-12-08 17:35:08 +0100

    day 8 part 1

---
+++ 2021/Day_08/part_1.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split(require("os").EOL);
+
+const pStart = performance.now();
+
+const RES = INPUT.reduce((p, c) => p + c.split(" | ")[1].split(" ").filter(({ length: l }) => ([2, 3, 4, 7].includes(l))).length, 0);
+
+const pEnd = performance.now();
+
+console.log("COUNT: " + RES);
+console.log(pEnd - pStart);
