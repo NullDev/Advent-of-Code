65a28c78ed73ce78b0693f145ee31b3028d933fe
NullDev (Chris) <shadow@nulldev.org>
2021-12-09 13:58:30 +0100

    day 9 part 1

---
+++ 2021/Day_09/part_1.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const READ = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split(require("os").EOL);
+const LEN = READ[0].length;
+const INPUT = READ.join("").split("").map(Number);
+
+const pStart = performance.now();
+
+let RES = INPUT.filter((e, i) => e < (INPUT[i - LEN] ?? 99)
+    && (i % LEN === LEN - 1 ? true : e < INPUT[i + 1])
+    && e < (INPUT[i + LEN] ?? 99)
+    && (i % LEN === 0 ? true : e < INPUT[i - 1])
+).reduce((p, c) => p + c + 1, 0);
+
+const pEnd = performance.now();
+
+console.log("SUM OF RISK LEVELS: " + RES);
+console.log(pEnd - pStart);
