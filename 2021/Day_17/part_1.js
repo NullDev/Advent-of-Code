e2f7cdd341b86404d6813dbad294212c0bd05519
NullDev (Chris) <shadow@nulldev.org>
2021-12-19 02:40:11 +0100

    Day 17

---
+++ 2021/Day_17/part_1.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim();
+
+const pStart = performance.now();
+
+// (-x - 1) * (-x) / 2
+// is equal to
+// ((1 / 8) * (((2 * x) + 1) ** 2)) - (1 / 8)
+// thus factorizing x, leaving us with a one-liner
+
+const RES = ((1 / 8) * (((2 * INPUT.match(/target area: x=(-?\d+)\.\.(-?\d+), y=(-?\d+)..(-?\d+)/).slice(1).map(Number)[2]) + 1) ** 2)) - (1 / 8);
+
+const pEnd = performance.now();
+
+console.log("HIGHEST Y POSITION: " + RES);
+console.log(pEnd - pStart);
