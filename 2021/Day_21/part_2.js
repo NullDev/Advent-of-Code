5c1f8667a198356df6914c3129f996a7195c7c47
NullDev (Chris) <shadow@nulldev.org>
2021-12-21 15:18:44 +0100

    Day 21

---
+++ 2021/Day_21/part_2.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+/* eslint-disable no-param-reassign, curly */
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim()
+    .split(require("os").EOL)
+    .flatMap(l => l.split(/\D+/).slice(-1).map(a => Number(a[0])));
+const STORE = {};
+
+const pStart = performance.now();
+
+let solve = (p, cp, np, cs = 0, rs = 0, e = [p, cp, np, cs, rs].join("|"), wins = [0, 0]) => (STORE[e]) ? STORE[e] : (() => {
+    for (let i = 1; i <= 3; i++)
+        for (let j = 1; j <= 3; j++)
+            for (let k = 1; k <= 3; k++) (cs + ((cp + (i + j + k) - 1) % 10) + 1 >= 21) ? wins[p]++ : solve(
+                1 - p, np, ((cp + (i + j + k) - 1) % 10) + 1, rs,
+                cs + ((cp + (i + j + k) - 1) % 10) + 1).forEach((el, idx) => (wins[idx] += el)
+            );
+    return (STORE[e] = wins);
+})();
+
+const RES = Math.max(...solve(0, ...INPUT, 0, 0));
+
+const pEnd = performance.now();
+
+console.log("WINNING PLAYER'S UNIVERSE COUNT: " + RES);
+console.log(pEnd - pStart);
