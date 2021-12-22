6d11991ab1570de5ad91d572c1ff75494d5f3384
NullDev <shadow@nulldev.org>
2021-12-22 16:05:36 +0100

    Day 22 (yet again, completely yeeted my first attempt)

---
+++ 2021/Day_22/part_1.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split(require("os").EOL).map((l, _, __, [cmd, bounds] = l.split(" ")) => ({
+    cmd,
+    cuboid: bounds.split(",").map(s => s.substring(2).split("..").map(Number))
+}));
+let splitter;
+
+const pStart = performance.now();
+
+const RES = INPUT.slice(0, 20).reduce((cub, instr) => [
+    ...cub.flatMap(c => (
+        (cuboid, other, intersect = cuboid.map((lim, i) => [Math.max(other[i][0], lim[0]), Math.min(other[i][1], lim[1])]).filter(([l, h]) => h >= l)) => (
+            (intersect.length === 3)
+                ? (splitter = (tmpCuboid, tmpIntersect, tmpC = 0) => [
+                    ...[
+                        [tmpCuboid[tmpC][0], tmpIntersect[tmpC][0] - 1], [tmpIntersect[tmpC][1] + 1, tmpCuboid[tmpC][1]]
+                    ].filter(([b1, b2]) => b2 >= b1).map(lim => Object.assign([...tmpCuboid], { [tmpC]: lim })),
+                    ...(tmpC < 2 ? splitter(Object.assign([...tmpCuboid], { [tmpC]: tmpIntersect[tmpC] }), tmpIntersect, tmpC + 1) : [])
+                ])(cuboid, intersect)
+                : [cuboid]
+        )
+    )(c, instr.cuboid)), ...(instr.cmd === "on" ? [instr.cuboid] : [])
+], []).map(c => c.reduce((r, [l, h]) => r * (h - l + 1), 1)).reduce((r, c) => c + r, 0);
+
+const pEnd = performance.now();
+
+console.log("CUBE COUNT (LOCAL): " + RES);
+console.log(pEnd - pStart);
