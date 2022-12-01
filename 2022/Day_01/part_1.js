3ead4829a5df3a5314dd3182b71fff2df0b92cc8
NullDev (Shadow) <chris@nulldev.org>
2022-12-01 10:15:12 +0100

    Solved day 1

--- 2022/Day_01/part_1.js
+++ 2022/Day_01/part_1.js
@@ -4,16 +4,13 @@ const fs = require("fs");
 const path = require("path");
 const { performance } = require("perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split(require("os").EOL); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split(require("os").EOL.repeat(2)).map(e => e.split(require("os").EOL).map(Number));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const result = Math.max(...INPUT.map(e => e.reduce((a, b) => a + b)));
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("Calories of Elf carrying the most: " + result);
 console.log(pEnd - pStart);
