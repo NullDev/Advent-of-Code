fedccbbfe4102a22ce4a4b4972b55c4851513f21
NullDev (Shadow) <chris@nulldev.org>
2022-12-02 10:53:00 +0100

    Solved day 2 🎉

--- 2022/Day_02/part_1.js
+++ 2022/Day_02/part_1.js
@@ -4,17 +4,16 @@ const fs = require("fs");
 const path = require("path");
 const { performance } = require("perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("
-"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+// feels a bit like cheating lol
+// but yea why not list every possible move with it's exact score
+const MOVES = { "A X": 4, "A Y": 8, "A Z": 3, "B X": 1, "B Y": 5, "B Z": 9, "C X": 7, "C Y": 2, "C Z": 6 };
+const score = INPUT.map(e => MOVES[e]).reduce((a, b) => a + b, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("FINAL SCORE (XYZ = ME): " + score);
 console.log(pEnd - pStart);
--- 2022/Day_02/part_2.js
+++ 2022/Day_02/part_2.js
@@ -4,17 +4,14 @@ const fs = require("fs");
 const path = require("path");
 const { performance } = require("perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("
-"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const MOVES = { "A X": 3, "A Y": 4, "A Z": 8, "B X": 1, "B Y": 5, "B Z": 9, "C X": 2, "C Y": 6, "C Z": 7 };
+const score = INPUT.map(e => MOVES[e]).reduce((a, b) => a + b, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("FINAL SCORE (XYZ = END): " + score);
 console.log(pEnd - pStart);
