79a7307467763d442044842108d195681611bc00
NullDev (Shadow) <chris@nulldev.org>
2022-12-04 06:46:56 +0100

    aint pretty but it works. solved day 4

--- 2022/Day_04/part_2.js
+++ 2022/Day_04/part_2.js
@@ -4,16 +4,14 @@ const fs = require("fs");
 const path = require("path");
 const { performance } = require("perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(l => l.split(","));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const result = INPUT.map(([r1, r2]) => [r1.split("-").map(Number), r2.split("-").map(Number)])
+    .filter(([r1, r2]) => (r1[0] <= r2[1] && r1[1] >= r2[0]) || (r2[0] <= r1[1] && r2[1] >= r1[0])).length;
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("OVERLAPS: " + result);
 console.log(pEnd - pStart);
