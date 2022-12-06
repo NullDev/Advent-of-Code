224482bfcaf863531613f190990a1a7f663e6fa4
NullDev (Shadow) <chris@nulldev.org>
2022-12-06 09:44:29 +0100

    solved day 6 🎉

--- 2022/Day_06/part_1.js
+++ 2022/Day_06/part_1.js
@@ -4,16 +4,13 @@ const fs = require("fs");
 const path = require("path");
 const { performance } = require("perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("");
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const result = INPUT.findIndex((_, i, l) => l.slice(i, i + 4).filter((x, y, z) => z.indexOf(x) === y).length === 4) + 4;
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("CHARS UNTIL FIRST START-OF-PACKET: " + result);
 console.log(pEnd - pStart);
