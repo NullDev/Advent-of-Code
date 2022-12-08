6b319c60933b16075a199bd10781cd2ab4102005
NullDev (Shadow) <chris@nulldev.org>
2022-12-08 19:19:00 +0100

    solved day 8 🎉

--- 2022/Day_08/part_1.js
+++ 2022/Day_08/part_1.js
@@ -1,19 +1,32 @@
 "use strict";
 
+/* eslint-disable no-param-reassign, no-return-assign */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n").map(row => row.split("").map(Number));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const visible = (line, _, __, top = -1) => line.map(tree => (tree > top ? (top = tree, true) : false));
+const t = m => m[0].map((_, i) => m.map(r => r[i]));
+
+const GRID = [
+    INPUT.map(visible),
+    INPUT.map(row => [...visible([...row].reverse())].reverse()),
+    t(t(INPUT).map(visible)),
+    t(t(INPUT).map(row => [...visible([...row].reverse())].reverse())),
+];
+
+const result = GRID[0].reduce(
+    (r, lR, rI) => r + lR.reduce(
+        (c, _, cI) => c + ((GRID[0][rI][cI] || GRID[1][rI][cI] || GRID[3][rI][cI] || GRID[2][rI][cI]) ? 1 : 0), 0,
+    ), 0,
+);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("VISIBLE TREES: " + result);
 console.log(pEnd - pStart);
--- 2022/Day_08/part_2.js
+++ 2022/Day_08/part_2.js
@@ -1,19 +1,32 @@
 "use strict";
 
+/* eslint-disable no-cond-assign */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n").map(row => row.split("").map(Number));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const scores = INPUT.map(row => row.map(() => 1));
+INPUT.forEach((e, i) => e.forEach((e2, j) => {
+    outer: // was wondering when I'm gonna use labels again :D
+    for (const [di, dj] of [[0, 1], [1, 0], [0, -1], [-1, 0]]){
+        if (di){
+            for (let i2 = i + di; i2 >= 0 && i2 < INPUT.length; i2 += di) if (e2 <= INPUT[i2][j] && (scores[i][j] *= Math.abs(i - i2))) continue outer;
+            scores[i][j] *= Math.abs(i - (di > 0 ? INPUT.length - 1 : 0));
+        }
+        else {
+            for (let j2 = j + dj; j2 >= 0 && j2 < e.length; j2 += dj) if (e2 <= e[j2] && (scores[i][j] *= Math.abs(j - j2))) continue outer;
+            scores[i][j] *= Math.abs(j - (dj > 0 ? e.length - 1 : 0));
+        }
+    }
+}));
+const result = Math.max(...scores.flat());
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("HIGHEST SCENIC SCORE: " + result);
 console.log(pEnd - pStart);
