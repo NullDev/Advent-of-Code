0a604d92b6341fa94fc8ee80e1bcd16de141580b
NullDev (Shadow) <chris@nulldev.org>
2022-12-09 06:55:59 +0100

    solved day 9 🎉

--- 2022/Day_09/part_1.js
+++ 2022/Day_09/part_1.js
@@ -4,16 +4,35 @@ const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim().split("\n").map(x => x.split(" ")).map(x => ({ dir: x[0], n: Number(x[1]) }));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const h = [0, 0];
+let t = [0, 0];
+const MM = {
+    L: [1, -1],
+    R: [1, 1],
+    U: [0, -1],
+    D: [0, 1],
+};
+
+const seen = {};
+seen[t[0] + "," + t[1]] = true;
+
+INPUT.forEach(m => {
+    for (let j = 0; j < m.n; j++){
+        const headPrev = h.map(x => x);
+        h[MM[m.dir][0]] += MM[m.dir][1];
+        if (Math.abs(h[0] - t[0]) > 1 || Math.abs(h[1] - t[1]) > 1) t = headPrev;
+        seen[t[0] + "," + t[1]] = true;
+    }
+});
+
+const result = Object.keys(seen).length;
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("POSITIONS: " + result);
 console.log(pEnd - pStart);
--- 2022/Day_09/part_2.js
+++ 2022/Day_09/part_2.js
@@ -1,19 +1,40 @@
 "use strict";
 
+/* eslint-disable curly */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim().split("\n").map(x => x.split(" ")).map(x => ({ dir: x[0], n: Number(x[1]) }));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const MM = {
+    L: [1, -1],
+    R: [1, 1],
+    U: [0, -1],
+    D: [0, 1],
+};
+const kn = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]];
+
+const seen = {};
+seen[kn[kn.length - 1][0] + "," + kn[kn.length - 1][1]] = true;
+
+INPUT.forEach(m => {
+    for (let j = 0; j < m.n; j++){
+        kn[0][MM[m.dir][0]] += MM[m.dir][1];
+        for (let k = 1; k < kn.length; k++)
+            if (Math.abs(kn[k][0] - kn[k - 1][0]) > 1 || Math.abs(kn[k][1] - kn[k - 1][1]) > 1)
+                ((kn[k][0] += Math.sign(kn[k - 1][0] - kn[k][0])) || 1) && (kn[k][1] += Math.sign(kn[k - 1][1] - kn[k][1]));
+        seen[kn[kn.length - 1][0] + "," + kn[kn.length - 1][1]] = true;
+    }
+});
+
+const result = Object.keys(seen).length;
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("POSITIONS: " + result);
 console.log(pEnd - pStart);
