26ea60d508241a0631213a2535c2f34ea7c9c76c
NullDev (Shadow) <chris@nulldev.org>
2022-12-10 07:02:49 +0100

    solved day 10

--- 2022/Day_10/part_1.js
+++ 2022/Day_10/part_1.js
@@ -4,16 +4,21 @@ const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(e => e.split(" "));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+let x = 1;
+let t = 0;
+let result = 0;
+
+INPUT.forEach(([inst, num]) => inst === "noop"
+    ? ((t += 1) || 1) && ([20, 60, 100, 140, 180, 220].includes(t)) && (result += x * t)
+    : (((t += 1) || 1) && ([20, 60, 100, 140, 180, 220].includes(t)) && (result += x * t) || 1)
+    && (((t += 1) || 1) && ([20, 60, 100, 140, 180, 220].includes(t)) && (result += x * t) || 1)
+    && (x += Number(num)));
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SUM OF SIGNAL STRENGTHS: " + result);
 console.log(pEnd - pStart);
--- 2022/Day_10/part_2.js
+++ 2022/Day_10/part_2.js
@@ -4,16 +4,22 @@ const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(e => e.split(" "));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const R = [Array(40).fill(" "), Array(40).fill(" "), Array(40).fill(" "), Array(40).fill(" "), Array(40).fill(" "), Array(40).fill(" ")];
+
+let x = 1;
+let t = 0;
+
+const d = (row = Math.floor(t / 40), col = t % 40) => ((x - 1 <= col && col <= x + 1) && (R[row][col] = "█") || 1);
+INPUT.forEach(([inst, num]) => inst === "noop"
+    ? d() && (t += 1)
+    : d() && ((t += 1) || 1) && d() && ((t += 1) || 1) && (x += Number(num)));
+const result = R.map(e => e.join("")).join("\n");
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("CRT IMAGE: " + (process.argv[2] === "t" ? "EABZCRJR" : ("\n" + result)));
 console.log(pEnd - pStart);
