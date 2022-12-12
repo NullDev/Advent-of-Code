6321265c57a163032223694426c1c0959d33ecfd
NullDev (Shadow) <chris@nulldev.org>
2022-12-12 10:29:33 +0100

    solved day 12 🎉

--- 2022/Day_12/README.md
+++ 2022/Day_12/README.md
@@ -38,3 +38,35 @@ In the above diagram, the symbols indicate whether the path exits each square mo
 This path reaches the goal in **`31`** steps, the fewest possible.
 
 **What is the fewest steps required to move from your current position to the location that should get the best signal?**
+
+---
+
+## --- Part Two ---
+
+As you walk up the hill, you suspect that the Elves will want to turn this into a hiking trail. The beginning isn't very scenic, though; perhaps you can find a better starting point.
+
+To maximize exercise while hiking, the trail should start as low as possible: elevation `a`. The goal is still the square marked `E`. However, the trail should still be direct, taking the fewest steps to reach its goal. So, you'll need to find the shortest path from **any square at elevation `a`** to the square marked `E`.
+
+Again consider the example from above:
+
+```
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+```
+
+Now, there are six choices for starting position (five marked `a`, plus the square marked `S` that counts as being at elevation `a`). If you start at the bottom-left square, you can reach the goal most quickly:
+
+```
+...v<<<<
+...vv<<^
+...v>E^^
+.>v>>>^^
+>^>>>>>^
+```
+
+This path reaches the goal in only **`29`** steps, the fewest possible.
+
+**What is the fewest steps required to move starting from any square with elevation `a` to the location that should get the best signal?**
--- 2022/Day_12/part_2.js
+++ 2022/Day_12/part_2.js
@@ -1,19 +1,42 @@
 "use strict";
 
+/* eslint-disable no-param-reassign, no-unused-vars */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim().split("\n").filter(x => x).map(x => x.split(""));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+let end = [];
+const start = [];
+
+const graph = INPUT.map((x, i) => x.map((
+    y, j, _, __ = (((y === "S") && (y = "a")) || ((y === "E") && ((end = [i, j]) && (y = "z")))),
+    ___ = ((y === "a") && start.push([i, j])),
+) => parseInt(y, 36)));
+
+const steps = graph.map(x => x.map(() => -1));
+start.forEach(x => (steps[x[0]][x[1]] = 0));
+
+const seen = start;
+while (seen.length > 0){
+    const pos = seen.pop() || [];
+    [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        .map(x => [pos[0] + x[0], pos[1] + x[1]])
+        .filter(x => x[0] >= 0 && x[0] < graph.length && x[1] >= 0 && x[1] < graph[0].length)
+        .filter(x => graph[x[0]][x[1]] - graph[pos[0]][pos[1]] <= 1)
+        .filter(x => steps[x[0]][x[1]] === -1)
+        .forEach(opt => (((steps[opt[0]][opt[1]] = (steps[pos[0]][pos[1]] + 1)) || 1) && seen.unshift(opt)));
+    if (pos[0] === end[0] && pos[1] === end[1]) break;
+}
+
+const result = steps[end[0]][end[1]];
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("FEWEST STEPS (ANY POS): " + result);
 console.log(pEnd - pStart);
