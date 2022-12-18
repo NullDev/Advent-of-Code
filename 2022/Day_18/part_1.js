6ead9bbb9641e29f762da8c443055291fade80d9
NullDev (Shadow) <chris@nulldev.org>
2022-12-18 08:13:01 +0100

    solved day 18 🎉

--- 2022/Day_18/part_1.js
+++ 2022/Day_18/part_1.js
@@ -1,19 +1,29 @@
 "use strict";
 
+/* eslint-disable curly, one-var */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n"),
+    MAP = {}, PTS = [];
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+INPUT.forEach((
+    e, _, __, p = e.split(",").map(x => +x),
+) => (PTS.push(p) && (MAP[`${p[0]}x${p[1]}x${p[2]}`] = 1)));
+
+const result = PTS.map(p => Number(!MAP[`${p[0] + 1}x${p[1]}x${p[2]}`])
+    + Number(!MAP[`${p[0] - 1}x${p[1]}x${p[2]}`])
+    + Number(!MAP[`${p[0]}x${p[1] + 1}x${p[2]}`])
+    + Number(!MAP[`${p[0]}x${p[1] - 1}x${p[2]}`])
+    + Number(!MAP[`${p[0]}x${p[1]}x${p[2] + 1}`])
+    + Number(!MAP[`${p[0]}x${p[1]}x${p[2] - 1}`]),
+).reduce((a, b) => a + b, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SURFACE AREA: " + result);
 console.log(pEnd - pStart);
--- 2022/Day_18/part_2.js
+++ 2022/Day_18/part_2.js
@@ -1,19 +1,47 @@
 "use strict";
 
+/* eslint-disable curly, one-var */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n"),
+    MAP = {}, PTS = [], M = [100000, -10000, 100000, -10000, 100000, -10000];
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+INPUT.forEach((
+    e, _, __, p = e.split(",").map(x => +x),
+) => (PTS.push(p) && (MAP[`${p[0]}x${p[1]}x${p[2]}`] = 1)));
+
+PTS.forEach(p => ((M[0] = Math.min(p[0], M[0])) || 1)
+    && ((M[1] = Math.max(p[0], M[1])) || 1)
+    && ((M[2] = Math.min(p[1], M[2])) || 1)
+    && ((M[3] = Math.max(p[1], M[3])) || 1)
+    && ((M[4] = Math.min(p[2], M[4])) || 1)
+    && ((M[5] = Math.max(p[2], M[5])) || 1),
+);
+
+(function f(x, y, z){
+    if (x < M[0] - 1 || x > M[1] + 1 || y < M[2] - 1 || y > M[3] + 1 || z < M[4] - 1 || z > M[5] + 1) return;
+    if (!MAP[`${x}x${y}x${z}`]) (
+        (MAP[`${x}x${y}x${z}`] = -1) || 1
+    ) && ( // @ts-ignore
+        f(x + 1, y, z) || f(x - 1, y, z) || f(x, y + 1, z)
+        || f(x, y - 1, z) || f(x, y, z + 1) || f(x, y, z - 1)
+    );
+})(M[0], M[2], M[4]);
+
+const result = PTS.map(p => Number(MAP[`${p[0] + 1}x${p[1]}x${p[2]}`] === -1)
+    + Number(MAP[`${p[0] - 1}x${p[1]}x${p[2]}`] === -1)
+    + Number(MAP[`${p[0]}x${p[1] + 1}x${p[2]}`] === -1)
+    + Number(MAP[`${p[0]}x${p[1] - 1}x${p[2]}`] === -1)
+    + Number(MAP[`${p[0]}x${p[1]}x${p[2] + 1}`] === -1)
+    + Number(MAP[`${p[0]}x${p[1]}x${p[2] - 1}`] === -1),
+).reduce((a, b) => a + b, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("EXTERIOR SURFACE AREA: " + result);
 console.log(pEnd - pStart);
