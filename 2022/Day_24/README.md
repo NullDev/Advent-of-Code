1d05d5058a4dd700fe90304e781a18ad1d23542a
NullDev (Shadow) <chris@nulldev.org>
2022-12-30 15:52:24 +0100

    day 24

--- 2022/Day_24/README.md
+++ 2022/Day_24/README.md
@@ -256,3 +256,17 @@ Minute 18, move down:
 </e.<<#></v<.^<#></e2></ve<2.#>```
 
 **What is the fewest number of minutes required to avoid the blizzards and reach the goal?**
+
+---
+
+## --- Part Two ---
+
+As the expedition reaches the far side of the valley, one of the Elves looks especially dismayed:
+
+He **forgot his snacks** at the entrance to the valley!
+
+Since you're so good at dodging blizzards, the Elves humbly request that you go back for his snacks. From the same initial conditions, how quickly can you make it from the start to the goal, then back to the start, then back to the goal?
+
+In the above example, the first trip to the goal takes `18` minutes, the trip back to the start takes `23` minutes, and the trip back to the goal again takes `13` minutes, for a total time of **`54`** minutes.
+
+**What is the fewest number of minutes required to reach the goal, go back to the start, then reach the goal again?**
--- 2022/Day_24/part_2.js
+++ 2022/Day_24/part_2.js
@@ -1,19 +1,49 @@
 "use strict";
 
+/* eslint-disable one-var, consistent-return */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(l => l.split(""));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const bMaps = [], dirs = [[-1,  0], [ 0, -1], [ 0,  0], [ 1,  0], [ 0,  1]];
+const gM = (t, sM = (a, b) => (a + b * 100000000) % b) => {
+    if (bMaps[t]) return bMaps[t];
+
+    const h = INPUT.length - 2, w = INPUT[0].length - 2, bmap = INPUT.map(l => l.map(v => v === "#" ? 1 : 0 ));
+    INPUT.forEach((l, y) => l.forEach((v, x) => {
+        if (v === "^") bmap[sM(y - 1 - t, h) + 1][x] = 1;
+        if (v === "v") bmap[((y - 1 + t) % h) + 1][x] = 1;
+        if (v === ">") bmap[y][((x - 1 + t) % w) + 1] = 1;
+        if (v === "<") bmap[y][sM(x - 1 - t, w) + 1] = 1;
+    }));
+
+    return (bMaps[t] = bmap);
+};
+
+const solve = (
+    start, end, t0, key = p => p.x + "_" + p.y + "_" + p.t,
+    paths = [{x: start.x, y: start.y, t: t0}], seen = new Set(),
+) => {
+    while (paths.length){
+        const p = paths.shift(); const k = key(p);
+        if (seen.has(k)) continue;
+        seen.add(k);
+        if (p?.y === end.y && p?.x === end.x) return p?.t;
+        const bmap = gM(p?.t + 1);
+        dirs.forEach(([dx, dy]) => bmap[p?.y + dy] && bmap[p?.y + dy][p?.x + dx] === 0
+        && paths.push({ y: p?.y + dy, x: p?.x + dx, t: p?.t + 1 }));
+    }
+};
+
+const s = { x: INPUT[0].indexOf("."), y: 0 }, e = { x: INPUT[INPUT.length - 1].indexOf("."), y: INPUT.length - 1 };
+const result = solve(s, e, solve(e, s, solve(s, e, 0)));
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("FEWEST NUMBER OF MINUTES TO REACH GOAL: " + result);
 console.log(pEnd - pStart);
