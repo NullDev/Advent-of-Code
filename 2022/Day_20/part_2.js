f837dde612bd8270957272d0897c1edb3205f36e
NullDev (Shadow) <chris@nulldev.org>
2022-12-30 13:00:36 +0100

    day 20

--- 2022/Day_20/part_2.js
+++ 2022/Day_20/part_2.js
@@ -1,19 +1,34 @@
 "use strict";
 
+/* eslint-disable curly */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(Number);
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const m = (num, d) => {
+    for (let time = 0; time < 10; time++)
+        d.forEach((
+            n, p, _, curr = num.findIndex(x => x.p === p),
+        ) => (num.splice(curr, 1) && num.splice((n + curr) % num.length, 0, { n, p })));
+    return num;
+};
+
+const result = [1000, 2000, 3000]
+    .map((
+        p, _, __, mx = m(
+            INPUT.map((n, i) => ({ n: n * 811589153, p: i })),
+            INPUT.map(num => num * 811589153),
+        ),
+        zi = mx.findIndex(num => num.n === 0),
+    ) => mx[(p + zi) % mx.length].n)
+    .reduce((p, c) => p + c, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SUM OF THREE GROVE COORDINATES (KEY): " + result);
 console.log(pEnd - pStart);
