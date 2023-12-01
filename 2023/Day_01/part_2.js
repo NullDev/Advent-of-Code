5ad4833ac2c988094198c3c09640e97b76046889
NullDev (Shadow) <chris@nulldev.org>
2023-12-01 09:43:01 +0100

    Solved D1P2

--- 2023/Day_01/part_2.js
+++ 2023/Day_01/part_2.js
@@ -8,12 +8,26 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+/* It could have been so easy, but no. We got edge cases with overlapping words...
+const result = INPUT.map(e => {
+    const r = e.replace(/one|two|three|four|five|six|seven|eight|nine/g, m => map[m]).replace(/\D/g, "");
+    return Number(r[0] + r.slice(-1));
+}).reduce((a, b) => a + b);
+*/
+
+const res = INPUT.map(e => {
+    const pairMat = [[Number.MAX_SAFE_INTEGER, 0], [-1, 0]];
+    ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].forEach((l, i) => {
+        const idxMat = [e.indexOf(String(i + 1)), e.lastIndexOf(String(i + 1)), e.indexOf(l), e.lastIndexOf(l)];
+        if (idxMat[0] >= 0 && idxMat[0] < pairMat[0][0]) pairMat[0] = [idxMat[0], i + 1];
+        if (idxMat[1] > pairMat[1][0]) pairMat[1] = [idxMat[1], i + 1];
+        if (idxMat[2] >= 0 && idxMat[2] < pairMat[0][0]) pairMat[0] = [idxMat[2], i + 1];
+        if (idxMat[3] > pairMat[1][0]) pairMat[1] = [idxMat[3], i + 1];
+    });
+    return Number(`${pairMat[0][1]}${pairMat[1][1]}`);
+}).reduce((a, b) => a + b);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SUM OF CALIBRATION VALUES: " + res);
 console.log(pEnd - pStart);
