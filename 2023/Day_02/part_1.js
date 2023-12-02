d0309e37778551af87e91f72f17be6c288c8c73a
NullDev (Shadow) <chris@nulldev.org>
2023-12-02 14:58:09 +0100

    Solved day 2

--- 2023/Day_02/part_1.js
+++ 2023/Day_02/part_1.js
@@ -1,19 +1,24 @@
 "use strict";
 
+/* eslint-disable no-return-assign, no-sequences */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT.filter(n => !!n).map(r => r.split(":")).map(e => ({
+    gn: Number(e[0].split("Game ")[1]),
+    tn: e[1].split(";").map(x => x.trim()).map(x => x.split(",")
+        .map(z => z.trim().split(" "))
+        .reduce((acc, y) => (acc[y[1]] = Number(y[0]), acc), {})),
+})).filter(row => row.tn.every(t => Object.keys(t).every(col => t[col] <= { red: 12, green: 13, blue: 14 }[col])))
+    .reduce((acc, row) => acc + row.gn, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SUM OF ID's: " + res);
 console.log(pEnd - pStart);
--- 2023/Day_02/part_2.js
+++ 2023/Day_02/part_2.js
@@ -1,19 +1,21 @@
 "use strict";
 
+/* eslint-disable no-param-reassign, no-return-assign, no-sequences */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n"); // change this if necessary
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT.filter(n => !!n).map(r => r.split(":")).map(e => (e[1].split(";").map(x => x.trim())
+    .map(x => x.split(",").map(z => z.trim().split(" ")).reduce((acc, y) => (acc[y[1]] = Number(y[0]), acc), {}))))
+    .map((tries) => tries.reduce((acc, t) => (Object.keys(t).forEach((c) => (acc[c] = Math.max(acc[c] || 0, t[c]))), acc)))
+    .reduce((acc, row) => (acc += Object.values(row).reduce((m, i) => m * i, 1)), 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SUM OF POWERS OF SET: " + res);
 console.log(pEnd - pStart);
