16386818ee64ca011ce86c54e8435f1a27332beb
NullDev (Shadow) <chris@nulldev.org>
2023-12-03 16:45:21 +0100

    finished day 3

--- 2023/Day_03/part_1.js
+++ 2023/Day_03/part_1.js
@@ -8,12 +8,22 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const [ nPos, sPos ] = [/[0-9]+/g, /[^0-9,\.]/g] // @ts-ignore
+    .map(reg => INPUT.reduce((data, str, l) => [
+        ...data,
+        ...([...str.matchAll(reg)].map(s => ({ l, t: s[0], pos: s.index }))),
+    ], []));
+
+const res = /** @type {any} */ (nPos).reduce((sum, { l: nL, pos: nP, t: nb }) =>
+    !! /** @type {any} */ (sPos).find(({ l: sL, pos: sP }) =>
+        (sL === nL ||
+          (!!(nL - 1) && sL === (nL - 1))
+          || (!!(nL + 1) && sL === (nL + 1))
+        ) && (sP <= nP + nb.length && sP >= (nP - 1)),
+    ) ? sum + (+nb) : sum, 0,
+);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SUM OF PART NUMBERS: " + res);
 console.log(pEnd - pStart);
--- 2023/Day_03/part_2.js
+++ 2023/Day_03/part_2.js
@@ -8,12 +8,23 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const [ nPos, sPos ] = [/[0-9]+/g, /\*/g] // @ts-ignore
+    .map(reg => INPUT.reduce((data, str, l) => [
+        ...data,
+        ...([...str.matchAll(reg)].map(s => ({ l, z: s[0], pos: s.index }))),
+    ], []));
+
+const res = /** @type {any} */ (sPos).reduce((sum, { l: sL, pos: sP }) => {
+    const n = /** @type {any} */ (nPos).filter(({ l: nL, pos: nP, z }) =>
+        (nL === sL ||
+            (!!(nL - 1) && sL === (nL - 1))
+            || (!!(nL + 1) && sL === (nL + 1))
+        ) && ((sP <= (nP + z.length)) && (sP >= (nP - 1))),
+    ).map(nbData => +nbData.z);
+    return n.length === 2 ? n.reduce((pr, nb) => nb * pr, 1) + sum : sum;
+}, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SUM OF GEAR RATIOS: " + res);
 console.log(pEnd - pStart);
