2d9242bd505739eba18f80e957e2e9eefeb71905
NullDev (Shadow) <chris@nulldev.org>
2023-12-05 07:00:22 +0100

    day 5 part 1

--- 2023/Day_05/part_1.js
+++ 2023/Day_05/part_1.js
@@ -8,12 +8,25 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const { maps } = INPUT.slice(1)
+    .reduce((acc, line) => {
+        if (line.trim() === "") return acc;
+        if (line.includes("map:")) (acc.currMap = line.split(":")[0]) && (acc.maps[acc.currMap] = []);
+        else {
+            const [dS, sS, rangeLen] = line.split(" ").map(Number);
+            acc.maps[acc.currMap].push({ dS, sS, rangeLen });
+        }
+        return acc;
+    }, { maps: {}, currMap: "" });
+
+const result = Math.min(
+    ...INPUT[0].split(": ")[1].split(" ").map(Number).map(seed => Object.keys(maps).reduce((number, mapName) => {
+        const mapping = maps[mapName].find(({ sS, rangeLen }) => number >= sS && number < sS + rangeLen);
+        return mapping ? mapping.dS + (number - mapping.sS) : number;
+    }, seed)),
+);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("LOWEST LOCATION NUMBER: " + result);
 console.log(pEnd - pStart);
