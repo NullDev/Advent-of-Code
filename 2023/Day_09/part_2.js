d1f16900fcfe31149f66a480c494156d347fa90b
NullDev (Shadow) <chris@nulldev.org>
2023-12-09 06:21:51 +0100

    solved day 9 part 2

--- 2023/Day_09/part_2.js
+++ 2023/Day_09/part_2.js
@@ -8,12 +8,12 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT.map(l => l.trim().split(" ").map(Number)).reduce((sum, h) => sum + ((seq = [h]) => {
+    while (seq[seq.length - 1].some(val => val !== 0)) seq.push(seq[seq.length - 1].map((v, i, arr) => i ? v - arr[i - 1] : v).slice(1));
+    return seq.reduceRight((next, seqN) => [seqN[0] - next[0], ...next])[0];
+})(), 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SUM OF VALUES: " + res);
 console.log(pEnd - pStart);
