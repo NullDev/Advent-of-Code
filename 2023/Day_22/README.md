6bf666264bd6ce8239bac9cc000794ddbd069c3c
NullDev (Shadow) <chris@nulldev.org>
2023-12-22 12:07:33 +0100

    solved day 22

--- 2023/Day_22/README.md
+++ 2023/Day_22/README.md
@@ -136,3 +136,20 @@ In this example, the bricks can be disintegrated as follows:
 So, in this example, **`5`** bricks can be safely disintegrated.
 
 Figure how the blocks will settle based on the snapshot. Once they've settled, consider disintegrating a single brick; **how many bricks could be safely chosen as the one to get disintegrated?**
+
+---
+
+## --- Part Two ---
+
+Disintegrating bricks one at a time isn't going to be fast enough. While it might sound dangerous, what you really need is a **chain reaction**.
+
+You'll need to figure out the best brick to disintegrate. For each brick, determine how many **other bricks would fall** if that brick were disintegrated.
+
+Using the same example as above:
+
+- Disintegrating brick `A` would cause all **`6`** other bricks to fall.
+- Disintegrating brick `F` would cause only **`1`** other brick, `G`, to fall.
+
+Disintegrating any other brick would cause **no other bricks** to fall. So, in this example, the sum of **the number of other bricks that would fall** as a result of disintegrating each brick is **`7`**.
+
+For each brick, determine how many **other bricks** would fall if that brick were disintegrated. **What is the sum of the number of other bricks that would fall?**
--- 2023/Day_22/part_1.js
+++ 2023/Day_22/part_1.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* eslint-disable no-sequences, one-var, no-param-reassign */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
@@ -8,12 +10,24 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const max = { x: -Infinity, y: -Infinity }, m = INPUT.map((
+        e, _, __, [, x1, y1, z1, x2, y2, z2] = (e.match(/(\d+),(\d+),(\d+)~(\d+),(\d+),(\d+)/) || []).map(Number),
+    ) => ((max.x = Math.max(max.x, x2)) && ( max.y = Math.max(max.y, y2)), {
+        a: { x: x1, y: y1, z: z1 }, b: { x: x2, y: y2, z: z2 }, f: 0, s: [], im: false,
+    })).sort((a, b) => a.a.z - b.a.z), size = { x: max.x + 1, y: max.y + 1 }, tv = new Array(size.x * size.y).fill(null);
+
+const res = m.map((e, bid, _, br = m[bid], { a, b } = br, fbb = 1, s = new Set()) => {
+    for (let { x } = a; x <= b.x; x++){
+        for (let { y } = a; y <= b.y; y++){
+            const fbi = tv[x + y * size.x]; // @ts-ignore
+            (((fbi !== null) && (((m[fbi].f + 1 > fbb) && (fbb = m[fbi].f + 1) && s.clear()) || 1)
+                && (m[fbi].f + 1 === fbb) && s.add(fbi)) || 1) && (tv[x + y * size.x] = bid);
+        }
+    } // @ts-ignore
+    return (((br.f = fbb + br.b.z - br.a.z) && (br.s = [...s])) || 1) && (br.s.length === 1) && (m[br.s[0]].im = true), e;
+}).filter(b => !b.im).length;
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SAFE BRICKS: " + res);
 console.log(pEnd - pStart);
