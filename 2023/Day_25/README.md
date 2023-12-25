2df8cfc55a6593e11c01513987add55cbce6f9f0
NullDev (Shadow) <chris@nulldev.org>
2023-12-25 17:24:47 +0100

    solved Day 25 - AOC 2k23 DONE!

--- 2023/Day_25/README.md
+++ 2023/Day_25/README.md
@@ -56,3 +56,25 @@ In this example, if you disconnect the wire between `hfx`/`pzl`, the wire betwee
 Multiplying the sizes of these groups together produces **`54`**.
 
 Find the three wires you need to disconnect in order to divide the components into two separate groups. **What do you get if you multiply the sizes of these two groups together?**
+
+---
+
+## --- Part Two ---
+
+You climb over weather machines, under giant springs, and narrowly avoid a pile of pipes as you find and disconnect the three wires.
+
+A moment after you disconnect the last wire, the big red reset button module makes a small ding noise:
+
+```
+System overload resolved!
+Power required is now 50 stars.
+```
+
+Out of the corner of your eye, you notice goggles and a loose-fitting hard hat peeking at you from behind an ultra crucible. You think you see a , but before you can investigate, you hear another small ding:
+
+```
+Power required is now 49 stars.
+
+Please supply the necessary stars and
+push the button to restart the system.
+```
--- 2023/Day_25/part_1.js
+++ 2023/Day_25/part_1.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* eslint-disable one-var, no-sequences */
+
 const fs = require("node:fs");
 const path = require("node:path");
 const { performance } = require("node:perf_hooks");
@@ -8,12 +10,30 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const G = new Map(), E = [];
+INPUT.map(l => l.split((/:?\s+/))).forEach(([f, ...n]) => n.forEach(t => (E.push([f, t]) && (G.has(f) || G.set(f, new Set())))
+    && (G.get(f).add(t) && (G.has(t) || G.set(t, new Set()))) && G.get(t).add(f)));
+
+// https://en.wikipedia.org/wiki/Karger%27s_algorithm
+
+const ck = (a, b) => a < b ? a + ":" + b : b + ":" + a, res = ( // @ts-ignore
+    (f, n = 1000, c = 3) => [...Array(n)].map(() => f()).find(([cut]) => (cut <= c))?.[1]
+)((
+    g = new Map([...G].map(([k, v]) => [k, new Set(v)])), e = E.slice(),
+    m = new Map(e.map(([a, b]) => [ck(a, b), 1])), nc = new Map([...G.keys()].map(n => [n, 1])),
+) => {
+    while (g.size > 2){
+        const [u, v] = ((i = Math.floor(Math.random() * e.length), ed = e[i], l = e.pop()) => (((i < e.length) && (e[i] = l)), ed))(); // @ts-ignore
+        ((!(!g.has(u) || !g.has(v))) && (((g.get(v) || []).forEach(vt => (vt !== u) && ((e.push(u < vt ? [u, vt] : [vt, u]) || 1)
+        && (g.get(u)?.add(vt) || 1) && (g.get(vt)?.delete(v) || 1) && (g.get(vt)?.add(u) || 1)
+            && m.set(ck(u, vt), (m.get(ck(u, vt)) || 0) + Number(m.get(ck(vt, v)))))) || 1)
+            && (g.get(u)?.delete(v) || 1) && (g.get(u)?.delete(u) || 1) && (g.delete(v) || 1) && (m.delete(ck(u, v)) || 1)
+        && (nc.set(u, Number(nc.get(u)) + Number(nc.get(v))) || 1) && nc.delete(v)));
+    }
+    return [m.get(ck(...g.keys())), [...nc.values()]]; // @ts-ignore
+})?.reduce((x, y) => x * y, 1);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("PRODUCT OF GROUP SIZES: " + res);
 console.log(pEnd - pStart);
