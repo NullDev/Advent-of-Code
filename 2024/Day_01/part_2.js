75791e4fbf82db5163060a10dbf19d70358193a8
NullDev (Shadow) <chris@nulldev.org>
2024-12-02 00:47:35 +0100

    optimize day 1 part 2

--- 2024/Day_01/part_2.js
+++ 2024/Day_01/part_2.js
@@ -8,13 +8,15 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
+/* eslint-disable one-var */
+
 const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(e => e.split(/\s+/).map(Number));
 
 const pStart = performance.now();
 
-const lists = [[], []]; // @ts-ignore
-for (const [left, right] of INPUT) lists[0].push(left) && lists[1].push(right);
-const f = lists[0].reduce((acc, curr) => ({ ...acc, [curr]: (acc[curr] || 0) + 1 }), {});
+const lists = [[], []], f = {}; // @ts-ignore
+for (const [left, right] of INPUT) lists[0].push(left) && lists[1].push(right); // @ts-ignore
+for (const curr of lists[0]) f[curr] = (f[curr] || 0) + 1;
 const result = lists[1].reduce((acc, curr) => acc + curr * (f[curr] || 0), 0);
 
 const pEnd = performance.now();
