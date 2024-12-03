3ad14c7cf532de851902e0ed3a0bfc3dcf34e4d2
NullDev (Shadow) <chris@nulldev.org>
2024-12-03 06:45:38 +0100

    day 3 p 2 solution

--- 2024/Day_03/part_2.js
+++ 2024/Day_03/part_2.js
@@ -12,12 +12,11 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const { n } = INPUT.reduce((acc, l) => [...l.matchAll(/(mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\)))/g)] // eslint-disable-next-line no-cond-assign
+    .reduce((state, m) => ((((m[1] === "don't()") && (state.on = 0)) || 1) && (m[1] === "do()") // eslint-disable-next-line no-sequences
+        ? (state.on = 1) : ((state.on) && (state.n += Number(m[2]) * Number(m[3]))), state), acc), { n: 0, on: 1 });
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("ENABLED MULS: " + n);
 console.log(pEnd - pStart);
