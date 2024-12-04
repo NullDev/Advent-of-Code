d61d2d18f09503bf622f50a3db0177762e09f917
NullDev (Shadow) <chris@nulldev.org>
2024-12-04 12:19:16 +0100

    solve d4 p1

--- 2024/Day_04/part_1.js
+++ 2024/Day_04/part_1.js
@@ -8,16 +8,18 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(l => l.split(""));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT.flatMap((row, i) => row.flatMap((_, j) => [
+    [INPUT[i][j], INPUT[i][j + 1], INPUT[i][j + 2], INPUT[i][j + 3]],
+    INPUT[i + 3] ? [INPUT[i][j], INPUT[i + 1][j], INPUT[i + 2][j], INPUT[i + 3][j]] : null,
+    INPUT[i + 3] ? [INPUT[i][j], INPUT[i + 1][j + 1], INPUT[i + 2][j + 2], INPUT[i + 3][j + 3]] : null,
+    INPUT[i + 3] ? [INPUT[i][j], INPUT[i + 1][j - 1], INPUT[i + 2][j - 2], INPUT[i + 3][j - 3]] : null,
+].filter(Boolean))).filter(word => (word?.join("") === "XMAS" || word?.join("") === "SAMX")).length;
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("XMAS COUNT: " + res);
 console.log(pEnd - pStart);
