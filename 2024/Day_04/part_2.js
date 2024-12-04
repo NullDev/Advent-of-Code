91c5393cc6a7c825902bc8532f774369c8dd1f94
NullDev (Shadow) <chris@nulldev.org>
2024-12-04 12:19:32 +0100

    solve d4 p2

--- 2024/Day_04/part_2.js
+++ 2024/Day_04/part_2.js
@@ -8,16 +8,23 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(l => l.split(""));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT.reduce((count, row, i, grid) => count + row.reduce((innerCount, cell, j) =>
+    cell === "A" && grid[i - 1] && grid[i + 1]
+        ? innerCount + (
+            (
+                (grid[i - 1][j - 1] + grid[i - 1][j + 1] === "MS" && grid[i + 1][j - 1] + grid[i + 1][j + 1] === "MS") ||
+                (grid[i - 1][j - 1] + grid[i - 1][j + 1] === "SM" && grid[i + 1][j - 1] + grid[i + 1][j + 1] === "SM") ||
+                (grid[i - 1][j - 1] + grid[i - 1][j + 1] === "SS" && grid[i + 1][j - 1] + grid[i + 1][j + 1] === "MM") ||
+                (grid[i - 1][j - 1] + grid[i - 1][j + 1] === "MM" && grid[i + 1][j - 1] + grid[i + 1][j + 1] === "SS")
+            ) ? 1 : 0
+        ) : innerCount, 0)
+, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("X-MAS COUNT: " + res);
 console.log(pEnd - pStart);
