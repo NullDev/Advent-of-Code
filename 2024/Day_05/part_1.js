1a9574ca9f659ad54c9c9da880450ac3181cad2a
NullDev (Shadow) <chris@nulldev.org>
2024-12-05 06:48:47 +0100

    Day 5 p1

--- 2024/Day_05/part_1.js
+++ 2024/Day_05/part_1.js
@@ -8,16 +8,22 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+/* eslint-disable one-var */
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n\n");
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const rules = INPUT[0].split("\n").map(
+        rule => ({ before: +rule.split("|")[0], after: +rule.split("|")[1] }),
+    ), res = INPUT[1].split("\n").map(u => u.split(",").map(Number))
+        .reduce((acc, update) => (
+            !rules.some(({ before, after }) => update.includes(before)
+                && update.includes(after)
+                && update.indexOf(before) >= update.indexOf(after))
+        ) ? acc + update[Math.floor(update.length / 2)] : acc, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("MIDDLE PAGE NUMBER SUM: " + res);
 console.log(pEnd - pStart);
