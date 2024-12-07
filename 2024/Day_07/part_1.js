6dce9869449d086802984fa3fccdb706ab6aa2e2
NullDev (Shadow) <chris@nulldev.org>
2024-12-07 06:33:27 +0100

    Day 7 part 1

--- 2024/Day_07/part_1.js
+++ 2024/Day_07/part_1.js
@@ -12,12 +12,18 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT.reduce((
+    a, b, _, __, [tar, nStr] = b.split(": "), num = nStr.split(" ").map(Number),
+    gen = (
+        length, ops = ['+', '*'], res = [],
+        g = (combo) => combo.length === length ? res.push(combo) : ops.forEach(op => g(combo + op))
+    ) => (g(''), res)
+) => gen(num.length - 1)
+    .some(ops => ops.split('').reduce((a, b, i) => b === '+' ? a + num[i + 1] : a * num[i + 1], num[0]) === Number(tar))
+        ? a + Number(tar)
+        : a, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("TOTAL CALIBRATIONS: " + res);
 console.log(pEnd - pStart);
