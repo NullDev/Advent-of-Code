877695b1f56ae87f96b5dd52a91c92e2314146f1
NullDev (Shadow) <chris@nulldev.org>
2024-12-08 10:24:45 +0100

    Day 8 p2

--- 2024/Day_08/part_2.js
+++ 2024/Day_08/part_2.js
@@ -8,16 +8,32 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+/* eslint-disable no-return-assign, no-sequences */
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim().split("\n").map(l => l.split(""));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = ((
+    [ ps, rows, cols ] = INPUT.reduce((acc, row, y) => (
+        row.forEach((v, x) => ((v === ".")
+            ? true : (acc[0][v] === undefined
+                ? acc[0][v] = [] : 0, acc[0][v].push([x, y]), 0))), acc
+    ), [{}, INPUT.length, INPUT[0].length]),
+) => Object.keys(
+    Object.entries(ps).reduce((pts, [, locs]) => (
+        locs.forEach((loc1, i) => locs.slice(i + 1).forEach((
+            loc2, _, __, d = [loc1[0] - loc2[0], loc1[1] - loc2[1]],
+        ) => [-1, 1].forEach((dir) =>
+            Array.from(
+                { length: Math.max(cols, rows) }, (___, step) => loc1.map((v, k) => v + dir * step * d[k]),
+            ).filter(([x, y]) => x >= 0 && y >= 0 && x < cols && y < rows).forEach(p => pts[p.join("_")] = 1),
+        ))), pts
+    ), {}),
+))().length;
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("NEW UNIQUE LOCATIONS: " + res);
 console.log(pEnd - pStart);
--- README.md
+++ README.md
@@ -131,6 +131,7 @@ I also tried to experiment with a couple of different things:
 - One-Liner in [Day_06/part_1.js](https://github.com/NullDev/Advent-of-Code/blob/master/2024/Day_06/part_1.js)
 - One-Liner in [Day_07/part_1.js](https://github.com/NullDev/Advent-of-Code/blob/master/2024/Day_07/part_1.js)
 - [DFS](https://en.wikipedia.org/wiki/Depth-first_search) with Memoization in [Day_07/part_2.js](https://github.com/NullDev/Advent-of-Code/blob/master/2024/Day_07/part_2.js)
+- One-Liner in both [Day_08/part_1.js](https://github.com/NullDev/Advent-of-Code/blob/master/2024/Day_08/part_1.js) and [Day_08/part_2.js](https://github.com/NullDev/Advent-of-Code/blob/master/2024/Day_08/part_2.js)
 
 <hr>
 
