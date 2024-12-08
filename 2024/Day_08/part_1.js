742a7728ba3e7df4c579aaddbb7dcfe73e704797
NullDev (Shadow) <chris@nulldev.org>
2024-12-08 10:24:28 +0100

    Day 8 p1

--- 2024/Day_08/part_1.js
+++ 2024/Day_08/part_1.js
@@ -8,16 +8,30 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+/* eslint-disable no-return-assign, no-sequences */
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim().split("\n").map(l => l.split(""));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = ((
+    [ ps, rows, cols ] = INPUT.reduce((acc, row, y) => (
+        row.forEach((v, x) => ((v === ".")
+            ? true : (acc[0][v] === undefined
+                ? acc[0][v] = [] : 0, acc[0][v].push([x, y]), 0))), acc
+    ), [{}, INPUT.length, INPUT[0].length]),
+) => Object.keys(
+    Object.entries(ps).reduce((acc, [, locs]) => (
+        locs.forEach((loc1, i) => locs.forEach((
+            loc2, j, _, onG = ([x, y]) => x >= 0 && y >= 0 && x < cols && y < rows,
+            d = [0, 1].map(i1 => loc1[i1] - loc2[i1]),
+            p1 = [0, 1].map(i1 => loc1[i1] + d[i1]), p2 = [0, 1].map(i1 => loc2[i1] - d[i1]), // @ts-ignore
+        ) => (!(j <= i) && (onG(p1) && (acc[p1.join("_")] = 1), onG(p2) && (acc[p2.join("_")] = 1))))), acc
+    ), {}),
+))().length;
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("UNIQUE LOCATIONS: " + res);
 console.log(pEnd - pStart);
