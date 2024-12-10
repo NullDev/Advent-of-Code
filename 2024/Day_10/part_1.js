61574f5ccec6e57c8ef16b0bfc3b7bb6290c252d
NullDev (Shadow) <chris@nulldev.org>
2024-12-10 16:02:18 +0100

    Day 10 part 1

--- 2024/Day_10/part_1.js
+++ 2024/Day_10/part_1.js
@@ -8,16 +8,27 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+/* eslint-disable no-sequences */
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(v => v.trim().split("").map(e => +e));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT.flat().map((v, i)=>[v, i % INPUT[0].length, i / INPUT[0].length | 0])
+    .filter(([v]) => v === 0).map(v => v.slice(1)).map(v => ((
+        map, pos,
+        get = (m, [x, y]) => m[y]?.[x],
+        set = (m, [x, y], w) => (m[y][x] = w),
+        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]],
+        seen = Array(map.length).fill(0).map((_, i) => Array(map[i].length).fill(0)),
+    ) => {
+        const dfs = (p, depth) => ((get(map, p) !== depth || get(seen, p))
+            ? 0 : set(seen, p, -1) && ((depth === 9) && set(seen, p, 1))
+                || dirs.map(w => dfs(p.map((e, i) => e + w[i]), depth + 1)));
+        return dfs(pos, 0), seen.flat().filter(w => w === 1).length;
+    })(INPUT, v)).reduce((a, b) => a + b);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SUM OF TRAILHEADS: " + res);
 console.log(pEnd - pStart);
