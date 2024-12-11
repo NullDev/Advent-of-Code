da8f4e4a4101b8fdc6800e33f46080bff5578f21
NullDev (Shadow) <chris@nulldev.org>
2024-12-11 06:38:53 +0100

    day 11 part 1

--- 2024/Day_11/part_1.js
+++ 2024/Day_11/part_1.js
@@ -8,16 +8,27 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+/* eslint-disable no-nested-ternary */
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split(" ").map(Number);
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const mem = {};
+const run = (
+    s, steps, key = `${s}|${steps}`,
+) => ((steps === 0) ? 1 : ((key in mem)
+    ? mem[key]
+    : (mem[key] = (s === 0)
+        ? run(1, steps - 1)
+        : (String(s).length % 2 === 0)
+            ? run(Number(String(s).substring(0, String(s).length / 2)), steps - 1)
+                + run(Number(String(s).substring(String(s).length / 2)), steps - 1)
+            : run(s * 2024, steps - 1)), mem[key]));
+
+const res = INPUT.map(s => run(s, 25)).reduce((acc, x) => acc + x);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("STONES AFTER BLINKING 25 TIMES: " + res);
 console.log(pEnd - pStart);
