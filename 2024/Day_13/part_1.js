34e177017f518074fc571e911641b8208d3a260c
NullDev (Shadow) <chris@nulldev.org>
2024-12-13 11:59:09 +0100

    day 13 part 1

--- 2024/Day_13/part_1.js
+++ 2024/Day_13/part_1.js
@@ -8,16 +8,18 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n\n").map(m => m.match(/\d+/g));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT.reduce(( // @ts-ignore
+    tok, [ax, ay, bx, by, tx1, ty1], _, __,
+    [tx, ty] = [tx1, ty1].map(Number),
+    a = (by * tx - bx * ty) / (ax * by - ay * bx),
+    b = (ax * ty - ay * tx) / (ax * by - ay * bx),
+) => (tok + ( !(a % 1 || b % 1) ? a * 3 + b : 0 )), 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("FEWEST POSSIBLE TOKENS: " + res);
 console.log(pEnd - pStart);
