df3448defac5b530afd8f773e90b86d9099945cb
NullDev (Shadow) <chris@nulldev.org>
2024-12-18 06:38:20 +0100

    day 18 part 1

--- 2024/Day_18/part_1.js
+++ 2024/Day_18/part_1.js
@@ -8,16 +8,35 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
+/* eslint-disable no-nested-ternary, no-shadow */
+
 const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = ((
+    grid = Array.from({ length: 71 }, () => Array(71).fill(false)),
+    pos = INPUT.map(line => line.split(",").map(Number)),
+    dir = [[0, 1], [0, -1], [1, 0], [-1, 0]],
+    seen = Array.from({ length: 71 }, () => Array(71).fill(false)),
+) => { // @ts-ignore
+    pos.forEach(([x, y], i) => i < 1024 && (grid[y][x] = true)) && (seen[0][0] = true);
+    const bfs = front =>
+        front.some(({x, y}) => x === 71 - 1 && y === 71 - 1)
+            ? front.find(({x, y}) => x === 71 - 1 && y === 71 - 1).dist
+            : front.length === 0 ? 0 : bfs(
+                front.flatMap(({x, y, dist}) =>
+                    dir.map(([dx, dy]) => ({ nx: x + dx, ny: y + dy, dist: dist + 1}))
+                        .filter(({ nx, ny }) => (
+                            nx >= 0 && nx < 71 && ny >= 0 && ny < 71
+                                && !grid[ny][nx] && !seen[ny][nx] && (seen[ny][nx] = true)
+                        )).map(({ nx, ny, dist }) => ({ x: nx, y: ny, dist })),
+                ),
+            );
+    return bfs([{ x: 0, y: 0, dist: 0 }]);
+})();
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("COORDINATES OF FIRST BLOCKED EXIT: " + res);
 console.log(pEnd - pStart);
