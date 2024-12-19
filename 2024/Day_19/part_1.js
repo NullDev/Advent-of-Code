97dbc0a6156c3a3b3a0d2cd937c5e7cb861a1103
NullDev (Shadow) <chris@nulldev.org>
2024-12-19 14:57:48 +0100

    day 19 part 1

--- 2024/Day_19/part_1.js
+++ 2024/Day_19/part_1.js
@@ -8,16 +8,24 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+/* eslint-disable no-return-assign, no-sequences */
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split(/\n\s*\n/)
+    .map(s => s.split("\n").map(e => e.trim()));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT[1].filter((
+    m, _, __,
+    pts = INPUT[0].flatMap(p => p.split(", ")),
+) => m.split("").reduce((dp, ___, i) => (
+    dp[i + 1] = pts.some(str => (
+        i + 1 >= str.length && dp[i + 1 - str.length]
+            && m.slice(i + 1 - str.length, i + 1) === str
+    )), dp
+), [true])[m.length]).length;
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("POSSIBLE DESIGNS: " + res);
 console.log(pEnd - pStart);
