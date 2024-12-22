de68c878ac6395381b6bf9e22d9c41595c484b82
NullDev (Shadow) <chris@nulldev.org>
2024-12-22 07:35:57 +0100

    day 22 part 1

--- 2024/Day_22/part_1.js
+++ 2024/Day_22/part_1.js
@@ -8,16 +8,21 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+/* eslint-disable curly, no-param-reassign */
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(Number);
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const total = INPUT.reduce((acc, e) => acc + ((
+    s, p = val => val >>> 0 & 0xFFFFFF, // OR val 2^24 = 16777216 (secret mod 2^24)
+) => {
+    for (let i = 0; i < 2000; i++) (s = p(s ^ p(s * 64))) // mul / div by n (64, 32, 2048), XOR, prune
+        && (s = p(s ^ p(Math.floor(s / 32)))) && (s = p(s ^ p(s * 2048)));
+    return s;
+})(e), 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("SUM OF 2000TH SECRET NUMBER: " + total);
 console.log(pEnd - pStart);
