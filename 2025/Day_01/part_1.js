11e844cadfdf91ff2bbc451173c8ff390519d42c
NullDev (Shadow) <chris@nulldev.org>
2025-12-01 14:26:14 +0100

    part 1 solution

--- 2025/Day_01/part_1.js
+++ 2025/Day_01/part_1.js
@@ -12,12 +12,15 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT.reduce((
+    [pos, count], row, _x, _y,
+    num = Number(row.slice(1)) % 100,
+    nPos = (100 + pos + (row[0] === "L" ? -num : num)) % 100,
+) => [
+    nPos, count + Number(nPos === 0),
+], [50, 0])[1];
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("DOOR PASSWORD: " + res);
 console.log(pEnd - pStart);
