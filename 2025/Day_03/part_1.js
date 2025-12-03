fe850714ea7c8dae9fe3a1e0c46449d1d278670c
NullDev (Shadow) <chris@nulldev.org>
2025-12-03 14:42:22 +0100

    part wan

--- 2025/Day_03/part_1.js
+++ 2025/Day_03/part_1.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import fs from "node:fs";
 import path from "node:path";
 import { performance } from "node:perf_hooks";
@@ -12,12 +13,21 @@ const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+// Should be O(n) methinks
+const result = INPUT.reduce((
+    acc, line, _, __, digits = line.trim().split("").map(Number),
+) => {
+    let rem = digits.length - 2;
+    const stack = digits.reduce((stk, d) => {
+        while (rem > 0 && stk.length && stk[stk.length - 1] < d) stk.pop() && rem--;
+        stk.push(d);
+        return stk;
+    }, []);
+    while (rem > 0) stack.pop() && rem--;
+    return acc + Number(stack.slice(0, 2).join(""));
+}, 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("OUTPUT JOLTAGE (2): " + result);
 console.log(pEnd - pStart);
