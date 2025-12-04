d74fd158ee562450effec02dc6f1f99c9efd783b
NullDev (Shadow) <chris@nulldev.org>
2025-12-04 06:51:33 +0100

    part 1 solution

--- 2025/Day_04/part_1.js
+++ 2025/Day_04/part_1.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import fs from "node:fs";
 import path from "node:path";
 import { performance } from "node:perf_hooks";
@@ -8,16 +9,24 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim()
+    .split("\n").map((line) => line.split(""));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = INPUT.reduce((t, row, y) => t + row.reduce(
+    (rt, c, x) => ((c !== "@")
+        ? rt : rt + (
+            [
+                [-1, -1], [-1,  0], [-1, 1], [0, -1],
+                [ 0,  1], [ 1, -1], [ 1, 0], [1,  1],
+            ].reduce((cnt, [dx, dy]) => (
+                cnt + (INPUT[y + dy]?.[x + dx] === "@" ? 1 : 0)
+            ), 0) < 4 ? 1 : 0
+        )), 0,
+), 0);
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("ACCESSIBLE ROLLS: " + res);
 console.log(pEnd - pStart);
