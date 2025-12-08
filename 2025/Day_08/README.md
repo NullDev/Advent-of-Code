011e1b1876fb3c810a7b664758f433aa07653868
NullDev (Shadow) <chris@nulldev.org>
2025-12-08 14:30:05 +0100

    d8 p1 sol

--- 2025/Day_08/README.md
+++ 2025/Day_08/README.md
@@ -57,3 +57,13 @@ This process continues for a while, and the Elves are concerned that they don't 
 After making the ten shortest connections, there are 11 circuits: one circuit which contains **5** junction boxes, one circuit which contains **4** junction boxes, two circuits which contain **2** junction boxes each, and seven circuits which each contain a single junction box. Multiplying together the sizes of the three largest circuits (5, 4, and one of the circuits of size 2) produces **`40`**.
 
 Your list contains many junction boxes; connect together the **1000** pairs of junction boxes which are closest together. Afterward, **what do you get if you multiply together the sizes of the three largest circuits?**
+
+---
+
+## --- Part Two ---
+
+The Elves were right; they **definitely** don't have enough extension cables. You'll need to keep connecting junction boxes together until they're all in **one large circuit**.
+
+Continuing the above example, the first connection which causes all of the junction boxes to form a single circuit is between the junction boxes at `216,146,977` and `117,168,530`. The Elves need to know how far those junction boxes are from the wall so they can pick the right extension cable; multiplying the X coordinates of those two junction boxes (`216` and `117`) produces **`25272`**.
+
+Continue connecting the closest unconnected pairs of junction boxes together until they're all in the same circuit. **What do you get if you multiply together the X coordinates of the last two junction boxes you need to connect?**
--- 2025/Day_08/part_1.js
+++ 2025/Day_08/part_1.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import fs from "node:fs";
 import path from "node:path";
 import { performance } from "node:perf_hooks";
@@ -8,16 +9,36 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+/* eslint-disable no-sequences */
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim().split("\n").map((x, xi) => [xi, x.split(",").map(Number)]);
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+const res = ((
+    dx = ([x1, y1, z1], [x2, y2, z2]) => (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2,
+    circs = Array(INPUT.length).fill(".").map((x, xi) => new Set([xi])),
+    dist = {},
+) => (((INPUT.values().forEach(
+    ([xi, xv]) => INPUT.filter((l, li) => li > xi).values().forEach(
+        ([ri, rv]) => (dist[dx(xv, rv)] = new Set([xi, ri])),
+    ),
+) || 1) && Object.values(dist)
+    .slice(0, 1000)
+    .forEach((
+        c, _, __,
+        cInds = [...c].map(x => circs.findIndex(y => y.has(x))),
+        cMin = cInds[0] < cInds[1] ? cInds[0] : cInds[1],
+        cMax = cMin === cInds[0] ? cInds[1] : cInds[0],
+    ) => (
+        (cInds[0] !== cInds[1]) && (
+            (circs[cMin] = circs[cMin].union(circs[cMax])) & circs.splice(cMax, 1)
+        )
+    ))), circs.map(x => x.size).sort((a, b) => b - a).slice(0, 3).reduce((a, c) => a * c, 1)
+))();
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("PRODUCT OF 3 LARGEST CIRCUITS: " + res);
 console.log(pEnd - pStart);
