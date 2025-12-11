f9915596d9f37d205bdd65ed7acd646a6b2e45f8
NullDev (Shadow) <chris@nulldev.org>
2025-12-11 13:50:55 +0100

    d11 p1 sol

--- 2025/Day_11/part_1.js
+++ 2025/Day_11/part_1.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import fs from "node:fs";
 import path from "node:path";
 import { performance } from "node:perf_hooks";
@@ -8,16 +9,22 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+/* eslint-disable no-sequences, no-return-assign, curly, no-param-reassign */
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim().split("\n").map(l => l.split(/[\:\s]/).filter(n => !!n));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+// Basic DFS
+const res = ((fromTo, heap = ["you"], visits = {}, top = null) => {
+    while (heap.length) (top = heap.pop()) && (
+        ((visits[top] = (visits[top] || 0) + 1) || 1) && heap.push(...(fromTo[top] || []))
+    );
+    return visits.out;
+})(INPUT.reduce((m, [from, ...tos]) => (m[from] = tos, m), {}));
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("YOU-TO-OUT PATHS: " + res);
 console.log(pEnd - pStart);
