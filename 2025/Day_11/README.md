a1fd2198bfa5d26d1d4f8ee607275e6caed40cac
NullDev (Shadow) <chris@nulldev.org>
2025-12-11 13:51:01 +0100

    d11 p2 sol

--- 2025/Day_11/README.md
+++ 2025/Day_11/README.md
@@ -45,3 +45,46 @@ In this example, these are all of the paths from `you` to `out`:
 In total, there are **`5`** different paths leading from `you` to `out`.
 
 **How many different paths lead from `you` to `out`?**
+
+---
+
+## --- Part Two ---
+
+Thanks in part to your analysis, the Elves have figured out a little bit about the issue. They now know that the problematic data path passes through both `dac` (a [digital-to-analog converter](https://en.wikipedia.org/wiki/Digital-to-analog_converter)) and `fft` (a device which performs a [fast Fourier transform](https://en.wikipedia.org/wiki/Fast_Fourier_transform)).
+
+They're still not sure which specific path is the problem, and so they now need you to find every path from `svr` (the server rack) to `out`. However, the paths you find must all also visit both `dac` **and** `fft` (in any order).
+
+For example:
+
+```
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+```
+
+This new list of devices contains many paths from `svr` to `out`:
+
+```
+svr,aaa,fft,ccc,ddd,hub,fff,ggg,out
+svr,aaa,fft,ccc,ddd,hub,fff,hhh,out
+svr,aaa,fft,ccc,eee,dac,fff,ggg,out
+svr,aaa,fft,ccc,eee,dac,fff,hhh,out
+svr,bbb,tty,ccc,ddd,hub,fff,ggg,out
+svr,bbb,tty,ccc,ddd,hub,fff,hhh,out
+svr,bbb,tty,ccc,eee,dac,fff,ggg,out
+svr,bbb,tty,ccc,eee,dac,fff,hhh,out
+```
+
+However, only **`2`** paths from `svr` to `out` visit both `dac` and `fft`.
+
+Find all of the paths that lead from `svr` to `out`. **How many of those paths visit both `dac` and `fft`?**
--- 2025/Day_11/part_2.js
+++ 2025/Day_11/part_2.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import fs from "node:fs";
 import path from "node:path";
 import { performance } from "node:perf_hooks";
@@ -8,16 +9,36 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+/* eslint-disable no-sequences, no-return-assign, no-loop-func, no-param-reassign */
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim().split("\n").map(l => l.split(/[\:\s]/).filter(n => !!n));
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+// Modified DFS
+const res = (fromTo => {
+    const fp = (
+        from, to, result = 0, heap = [from], visits = { [from]: 1 },
+        top = null, vc = 0,
+    ) => {
+        while (heap.length){
+            (top = heap.pop()) && (vc = visits[top]);
+            delete visits[top];
+            if (top === to) result += vc;
+            (fromTo[top] || []).forEach(t => (
+                (visits[t])
+                    ? visits[t] += vc
+                    : ((visits[t] = vc) || 1) && heap.unshift(t)
+            ));
+        }
+        return result;
+    };
+    return (fp("svr", "dac") * fp("dac", "fft") * fp("fft", "out"))
+         + (fp("svr", "fft") * fp("fft", "dac") * fp("dac", "out"));
+})(INPUT.reduce((m, [from, ...tos]) => (m[from] = tos, m), {}));
 
 const pEnd = performance.now();
 
-console.log("<DESCRIPTION>: " + result);
+console.log("PATHS THAT VISIT DAC AND FFT: " + res);
 console.log(pEnd - pStart);
